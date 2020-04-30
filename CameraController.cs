95d7509bb25a2e60de0f82dc96f1e30743c1e979
alex <alex@codeforage.com>
2020-04-30 11:56:54 -0300

    Adding repo

---
+++ CameraController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public float turnSpeed = 4.0f;
+    public Transform objectToOrbit;
+    private Vector3 offset;
+    private Quaternion initialRotation;
+    private Vector3 initialPosition;
+    GameObject rayerCaster;
+     void Start () {
+        rayerCaster = GameObject.Find("RayCaster");
+		offset = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z);
+        initialRotation = new Quaternion(transform.localRotation.x,transform.localRotation.y,transform.localRotation.z, transform.localRotation.w);
+        initialPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z);
+     }
+
+    void FixedUpdate(){
+        Vector3 fwd = rayerCaster.transform.TransformDirection(Vector3.forward);//Get our forward dir
+        Debug.DrawLine(rayerCaster.transform.position, fwd * 50000000, Color.red);//Points towards object based on LateUpdate code.
+       
+        
+    }
+    private void OnTriggerEnter(Collider other)
+	{
+          Debug.Log("In tag type: " + other.gameObject.tag);
+        if(other.gameObject.tag =="Terrain"){
+             //We now need to use the ray cast to check if we are clipping and essentially move in the opposite direction until no contact
+            Debug.Log("In terrain type: " + other.gameObject.name);
+        }
+
+    }
+     void LateUpdate()
+     {
+
+         //Keep camera from flipping with car.
+		if (Input.GetKey (KeyCode.LeftControl)) {	
+			// offset = Quaternion.AngleAxis (Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+            offset = Quaternion.AngleAxis (Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+            transform.position = objectToOrbit.localPosition + offset; 
+			transform.LookAt(objectToOrbit.localPosition);
+
+		}else{
+            transform.localRotation = initialRotation;
+            transform.localPosition = initialPosition;     
+        }
+
+        transform.rotation = Quaternion.LookRotation(objectToOrbit.position - transform.position, Vector3.up); //Keep our position on top instead of flipping with object
+        
+     
+     }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Unity Camera Controller (Third Person)
+
+**Camera Controller functionality**
+
+This controller will allow you to rotate around a given object with ctrl + mouse movement around the X axis. 
+
+**Issues**
+
+Camera will clip into the ground if object is at an awkward angle. Working on adding trigger collider to camera as well as raycasting
+to determine how to move away from an object to avoid clipping.
